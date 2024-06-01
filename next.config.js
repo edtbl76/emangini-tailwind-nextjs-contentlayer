ab5e7704dff988ff1fc003885fdc47a9621844b4
Ed and Gravy <me@emangini.com>
2024-06-01 18:14:15 -0400

    adding f.convertkit.com to the CSP (What a pain in the ass.)

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is http://emangini.ck.page;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is https://emangini.ck.page https://f.convertkit.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
