c85ea50f5d167522109773b191938d75dd7e3247
Ed and Gravy <me@emangini.com>
2024-06-01 17:24:56 -0400

    - going back to trying it as a script

--- components/NewsletterGroveForm.tsx
+++ components/NewsletterGroveForm.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import Head from 'next/head'
+import Script from 'next/script'
 
 const NewsletterGroveForm: React.FC = () => {
   return (
-    <Head>
-      <script
-        async
-        data-uid="576a6b3274"
+    <div>
+      <Script
         src="https://emangini.ck.page/576a6b3274/index.js"
-      ></script>
-    </Head>
+        strategy="lazyOnload"
+        data-uid="576a6b3274"
+        async
+      />
+    </div>
   )
 }
 
