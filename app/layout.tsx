0274d04c7a7de2cb66acbe59818b2d55e5c8a2a3
Ed and Gravy <me@emangini.com>
2024-06-01 16:41:29 -0400

    - fixed path to favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,16 +64,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       className={`${space_grotesk.variable} scroll-smooth`}
       suppressHydrationWarning
     >
-      <link rel="icon" href="/favicons/favicon.ico" />
-      <link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+      <link rel="icon" href="/static/favicons/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
       <link
         rel="manifest"
         href="/static/favicons/site.webmanifest"
         crossOrigin={'use-credentials'}
       />
-      <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+      <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
