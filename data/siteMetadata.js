9b0add7f2af2fd5c194cd14f478d6af5ba90bc5d
Ed and Gravy <me@emangini.com>
2024-06-01 00:52:24 -0400

    - removed surplus social media

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -17,11 +17,11 @@ const siteMetadata = {
   github: 'https://github.com/edtbl76',
   x: 'https://twitter.com/edward_mangini',
   // twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
+  // facebook: 'https://facebook.com',
+  // youtube: 'https://youtube.com',
   linkedin: 'https://www.linkedin.com/in/edwardmangini',
-  threads: 'https://www.threads.net',
-  instagram: 'https://www.instagram.com',
+  // threads: 'https://www.threads.net',
+  // instagram: 'https://www.instagram.com',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
