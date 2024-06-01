ea0477998662dfe3557f1ffa3d6236c9f5b68392
Ed and Gravy <me@emangini.com>
2024-06-01 12:28:58 -0400

    - added Microdaddy

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -12,7 +12,16 @@ const projectsData: Project[] = [
     education and transparent governance, empowering communities and driving 
     positive societal and environmental impacts.`,
     imgSrc: '/static/images/devvocates.png',
-    href: 'https://github.com/devvocates',
+    href: 'https://devvocatres.org',
+  },
+  {
+    title: 'microdaddy',
+    description: `This is an example of microservices on k8s. The commit history
+    shows the journey through different time periods of service orientation. 
+    Naming is hard. My son asked me what I was doing. I told him I was writing 
+    microservices. He said 'Microdaddy!'`,
+    imgSrc: '/static/images/microdaddy.png',
+    href: 'https://github.com/edtbl76/microdaddy',
   },
 ]
 
