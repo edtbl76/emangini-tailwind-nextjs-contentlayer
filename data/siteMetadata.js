7373f88cb3bff84d015dcd3514f95d4d6c73b1a1
Ed and Gravy <me@emangini.com>
2024-06-02 13:16:53 -0400

    Trying to add a new line to the home page description.

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -4,8 +4,7 @@ const siteMetadata = {
   author: 'Edward Mangini',
   headerTitle: '@emangini',
   description:
-    'This is where I unpack thoughts and research on technology, business and leadership. ' +
-    '\nNo fluff, just the good stuff.',
+    'This is where I unpack thoughts and research on technology, business and leadership.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
