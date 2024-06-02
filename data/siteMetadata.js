0742f307b1f6c91062915ccef85359a4920fa07e
Ed and Gravy <me@emangini.com>
2024-06-02 02:27:30 -0400

    Trying to add a new line to the home page description.

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,7 +5,7 @@ const siteMetadata = {
   headerTitle: '@emangini',
   description:
     'This is where I unpack thoughts and research on technology, business and leadership. ' +
-    'No fluff, just the good stuff.',
+    '\nNo fluff, just the good stuff.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
