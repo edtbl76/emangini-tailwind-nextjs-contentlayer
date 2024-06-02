956dc08bf4739a399897bda449ae857957324ba7
Ed and Gravy <me@emangini.com>
2024-06-01 22:11:51 -0400

    adding more recaptcha to the CSP (Still a pain in the ass.)

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is https://emangini.ck.page https://f.convertkit.com https://www.google.com/recaptcha/;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is https://emangini.ck.page https://f.convertkit.com https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
