fc54542df39c3c02d91b0c4fa95412c24bd49e3a
Ed and Gravy <me@emangini.com>
2024-06-03 15:46:39 -0400

    - reverted search integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,6 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
-import { KBarCustomSearchProvider } from '@/components/KBarCustomSearchProvider'
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -84,14 +83,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
             <div className="flex h-screen flex-col justify-between font-sans">
-              {/*<SearchProvider searchConfig={siteMetadata.search as SearchConfig}>*/}
-              {/*  <Header />*/}
-              {/*  <main className="mb-auto">{children}</main>*/}
-              {/*</SearchProvider>*/}
-              <KBarCustomSearchProvider>
+              <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
                 <Header />
-                <main className={'mb-auto'}>{children}</main>
-              </KBarCustomSearchProvider>
+                <main className="mb-auto">{children}</main>
+              </SearchProvider>
               <Footer />
             </div>
           </SectionContainer>
