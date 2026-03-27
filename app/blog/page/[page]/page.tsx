9de3f797b04a6e397397589f2672a2f583d6f993
Edward Mangini <me@emangini.com>
2026-03-27 18:11:28 -0400

    chore: update page routes to use velite content

--- app/blog/page/[page]/page.tsx
+++ app/blog/page/[page]/page.tsx
@@ -1,18 +1,17 @@
-import ListLayout from '@/layouts/ListLayoutWithTags'
-import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
-// @ts-ignore
-import { allBlogs } from 'contentlayer/generated'
+import ListLayout from '@/layouts/ListLayout'
+import { allCoreContent, sortPosts } from '@/lib/content-utils'
+import { blogs } from '@/content'
 
 const POSTS_PER_PAGE = 5
 
 export const generateStaticParams = async () => {
-  const totalPages = Math.ceil(allBlogs.length / POSTS_PER_PAGE)
+  const totalPages = Math.ceil(blogs.length / POSTS_PER_PAGE)
   return Array.from({ length: totalPages }, (_, i) => ({ page: (i + 1).toString() }))
 }
 
 export default async function Page(props: { params: Promise<{ page: string }> }) {
   const params = await props.params
-  const posts = allCoreContent(sortPosts(allBlogs))
+  const posts = allCoreContent(sortPosts(blogs))
   const pageNumber = parseInt(params.page as string)
   const initialDisplayPosts = posts.slice(
     POSTS_PER_PAGE * (pageNumber - 1),
